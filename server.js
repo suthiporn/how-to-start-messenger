7bc2efb88638dce0c4bb8caaf81c68c2bcc3e198
suthiporn <job_30720@hotmail.com>
2016-12-11 09:43:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-const token = 'key'
+const token = 'EAAKeYHAxsSkBAFiHA6CptSDYeZClNfwFdLBxZAIoAeR6iO7UMBCF6KsfDFluS3qDgWiZA1EYwwRIiwtbrRd3zHwiwTw8UE8E0u2omq7NT2WSvWD7ZACa4anOuf7YDKh2ZCfl3tAnvVFxh8wQD7tfP7Rb6luUDqGimpecmgkEV9wZDZD'
 app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
   res.send('test test')
 })
 app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === '') {
+  if (req.query['hub.verify_token'] === 'botweather') {
     res.send(req.query['hub.challenge'])
   }
   res.send('Error, wrong token')
