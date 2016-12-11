365e0fc0f5899820911dabb7275ab386702cbfb1
suthiporn <job_30720@hotmail.com>
2016-12-11 10:42:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,20 @@ app.post('/webhook/', function (req, res) {
     let sender = event.sender.id
     if (event.message && event.message.text) {
       let text = event.message.text
+      var location = event.message.text
+      var weatherEndpoint = 'http://api.openweathermap.org/data/2.5/weather?q=' +location+ '&units=metric&appid=a91c6f96a68f517cbf1e7e3960735891'
+      request({
+        url: weatherEndpoint,
+        json: true
+      }, function(error, response, body) {
+        try {
+          var condition = body.main;
+          sendTextMessage(sender, "Today is " + condition.temp + "Celsius in " + location);
+        } catch(err) {
+          console.error('error caught', err);
+          sendTextMessage(sender, "There was an error.");
+        }
+      })
       if (text === 'Generic') {
         sendGenericMessage(sender)
         continue
