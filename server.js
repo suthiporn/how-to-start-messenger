cafd3fdb5672fb628ecc7f7ac5bcaf0048207fd4
suthiporn <job_30720@hotmail.com>
2016-12-11 10:29:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.post('/webhook/', function (req, res) {
         sendGenericMessage(sender)
         continue
       }
-      sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200))
+      sendTextMessage(sender, 'มาเช็คสภาพอากาศกันเถอะ' + text.substring(0, 200))
     }
     if (event.postback) {
       let text = JSON.stringify(event.postback)
